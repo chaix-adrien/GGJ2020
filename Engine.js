c2defd0132b85779f30dd968d886b8530283f814
orafrost <guillaume@MacBook-Pro-de-verrier.local>
2020-02-01 18:51:04 +0100

    update

--- Engine.js
+++ Engine.js
@@ -1,10 +1,10 @@
-function Engine(cardPool){
+function Engine(cardPool, enemi = [], player){
   this.turn = 0;
   this.cardPool = cardPool;
-  console.log(this.cardPool)
-  this.player = new Personnage("carlito", 30, 5);
+  this.player = player
   this.partie = new Partie(this.cardPool);
   this.partie.giveCard();
+  this.enemi = enemi;
 }
 
 Engine.prototype.getCard = function() {
@@ -14,12 +14,27 @@ Engine.prototype.getCard = function() {
 }
 
 Engine.prototype.action = function (cardId, target) {
+  console.log(this);
   card = this.partie.play(cardId);
   if (this.player.play(card.cost)){
       card.action(target);
+      this.turn += 1;
+      if (this.partie.hand.length === 0) {
+        this.partie.giveCard();
+      }
+      return true;
   }
+  return false;
+}
+
+Engine.prototype.choixCard = function () {
 
 }
+
+Engine.prototype.next = function () {
+
+}
+
 
 Engine.prototype.end = function() {
   if ( (this.player.is_alive() === false)   || (this.player.mana === 0 )) {
