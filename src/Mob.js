e6b3e99c29892279fe872bd7c4d1a673169dee46
Adrien Chaix <chaix.adrien@laposte.net>
2020-02-02 13:26:27 +0100

    auto ad mob

--- src/Mob.js
+++ src/Mob.js
@@ -1,31 +1,36 @@
 import DE from '@dreamirl/dreamengine';
 
-export default (Game = window.Game) => new DE.GameObject({
-  interactive: true,
-  x: 1920 / 2,
-  y: 200,
-  targeted: false,
-  pointerover: function () {
-    if (Game.selectedCard && !this.targeted && Game.selectedCard.needTarget) {
-      this.targeted = true
-      this.addAutomatism('animTargeted', "_animTargeted", { interval: 10, value1: this })
-    }
-  },
-  _animTargeted: function () {
-    this.shake(10, 10, 100)
-    this.scale = { x: 1.2, y: 1.2 }
-  },
-  pointerup: function () {
-    this.pointerout()
-    if (Game.selectedCard && Game.waitingForPlay && Game.selectedCard.needTarget) {
-      console.log("send", this)
-      Game.waitingForPlay(Game.selectedCard, this)
-    }
-  },
-  pointerout: function () {
-    this.targeted = false
-    this.scale = { x: 1, y: 1 }
-    this.removeAutomatism("animTargeted")
-  },
-  renderer: new DE.SpriteRenderer({ spriteName: 'heart', scale: 1 }),
-});
+export default (Game = window.Game) => {
+  const obj = new DE.GameObject({
+    interactive: true,
+    x: 1920 / 2,
+    y: 200,
+    targeted: false,
+    pointerover: function () {
+      if (Game.selectedCard && !this.targeted && Game.selectedCard.needTarget) {
+        this.targeted = true
+        this.addAutomatism('animTargeted', "_animTargeted", { interval: 10, value1: this })
+      }
+    },
+    _animTargeted: function () {
+      this.shake(10, 10, 100)
+      this.scale = { x: 1.2, y: 1.2 }
+    },
+    pointerup: function () {
+      this.pointerout()
+      if (Game.selectedCard && Game.waitingForPlay && Game.selectedCard.needTarget) {
+        console.log("send", this)
+        Game.waitingForPlay(Game.selectedCard, this)
+      }
+    },
+    pointerout: function () {
+      this.targeted = false
+      this.scale = { x: 1, y: 1 }
+      this.removeAutomatism("animTargeted")
+    },
+    renderer: new DE.SpriteRenderer({ spriteName: 'heart', scale: 1 }),
+  })
+
+  Game.scene.add(obj)
+  return obj
+};
